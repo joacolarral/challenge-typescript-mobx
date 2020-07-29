6337be000cfba9621a757f1e99b47d34531f4d83
joacolarral <joacolarral@gmail.com>
2020-07-29 15:45:04 -0300

    Updates

---
+++ precache-manifest.8345ae916c5bafba787f7f0d824ca7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f9fa63420a18aaa6f5d1cce628dc1b",
+    "url": "/challenge-typescript-mobx/index.html"
+  },
+  {
+    "revision": "15968f65d704d5f15075",
+    "url": "/challenge-typescript-mobx/static/css/main.156ff9d9.chunk.css"
+  },
+  {
+    "revision": "de2336b749198c66a63c",
+    "url": "/challenge-typescript-mobx/static/js/2.cec4f1c4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/challenge-typescript-mobx/static/js/2.cec4f1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15968f65d704d5f15075",
+    "url": "/challenge-typescript-mobx/static/js/main.a71fb4d4.chunk.js"
+  },
+  {
+    "revision": "41a4fb3fd0b70c732a8b",
+    "url": "/challenge-typescript-mobx/static/js/runtime-main.2b8256e6.js"
+  }
+]);
